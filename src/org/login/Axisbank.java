05bb53fa5ec602cf8e23f3dd4b76a1d737565b78
senthamizhmaran <Zentthamizhmaran10@gmail.com>
2024-12-05 16:37:32 +0530

    update on senthamizh branch

--- src/org/login/Axisbank.java
+++ src/org/login/Axisbank.java
@@ -5,7 +5,7 @@ public abstract class Axisbank {
 	
 	abstract void fixed();
 withdarw
-	public void minimumDeposite() {
+	private main maximumdeposite() {
 		System.out.println("minimum deposite amount is rs 1000");
 		
 		System.out.println("testing git");
